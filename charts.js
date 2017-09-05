0e0a3131d89f8c02975607b507dc2c65da5fd2be
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-05 17:43:24 +1000

    drawable graph done

--- charts.js
+++ charts.js
@@ -40,58 +40,129 @@ function generateChartData(data) {
 
 function generateChart(chartData) {
   var chart = AmCharts.makeChart( "chartdiv", {
-  "type": "serial",
+  "type": "stock",
   "theme": "light",
-  "dataDateFormat":"YYYY-MM-DD",
-  "valueAxes": [ {
-    "position": "left"
+  "dataSets": [ {
+    "color": "#b0de09",
+    "fieldMappings": [ {
+      "fromField": "value",
+      "toField": "value"
+    } ],
+    "dataProvider": chartData,
+    "categoryField": "date"
   } ],
-  "graphs": [ {
-    "id": "g1",
-    "proCandlesticks": true,
-    "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
-    "closeField": "close",
-    "fillColors": "#7f8da9",
-    "highField": "high",
-    "lineColor": "#7f8da9",
-    "lineAlpha": 1,
-    "lowField": "low",
-    "fillAlphas": 0.9,
-    "negativeFillColors": "#db4c3c",
-    "negativeLineColor": "#db4c3c",
-    "openField": "open",
-    "title": "Price:",
-    "type": "candlestick",
-    "valueField": "close"
+
+  "panels": [ {
+    "showCategoryAxis": true,
+    "title": "Value",
+    "eraseAll": false,
+    "allLabels": [ {
+      "x": 0,
+      "y": 115,
+      "text": "Click on the pencil icon on top-right to start drawing",
+      "align": "center",
+      "size": 16
+    } ],
+
+    "stockGraphs": [ {
+      "id": "g1",
+      "valueField": "value",
+      "useDataSetColors": false
+    } ],
+
+    "stockLegend": {
+      "valueTextRegular": " ",
+      "markerType": "none"
+    },
+
+    "drawingIconsEnabled": true
   } ],
-  "chartScrollbar": {
-    "graph": "g1",
-    "graphType": "line",
-    "scrollbarHeight": 30
+
+  "chartScrollbarSettings": {
+    "graph": "g1"
   },
-  "chartCursor": {
-    "valueLineEnabled": true,
-    "valueLineBalloonEnabled": true
+  "chartCursorSettings": {
+    "valueBalloonsEnabled": true
   },
-  "categoryField": "date",
-  "categoryAxis": {
-    "parseDates": true
-  },
-  "dataProvider": chartData,
-  "export": {
-    "enabled": true,
-    "position": "bottom-right"
+  "periodSelector": {
+    "position": "bottom",
+    "periods": [ {
+      "period": "DD",
+      "count": 10,
+      "label": "10 days"
+    }, {
+      "period": "MM",
+      "count": 1,
+      "label": "1 month"
+    }, {
+      "period": "YYYY",
+      "count": 1,
+      "label": "1 year"
+    }, {
+      "period": "YTD",
+      "label": "YTD"
+    }, {
+      "period": "MAX",
+      "label": "MAX"
+    } ]
   }
 } );
-
-  chart.addListener( "rendered", zoomChart );
-  zoomChart();
-  // this method is called when chart is first inited as we listen for "dataUpdated" event
-  function zoomChart() {
-    // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-    chart.zoomToIndexes( chart.dataProvider.length - 50, chart.dataProvider.length - 1 );
-  }
 }
+
+// function generateChart(chartData) {
+//   var chart = AmCharts.makeChart( "chartdiv", {
+//   "type": "serial",
+//   "theme": "light",
+//   "dataDateFormat":"YYYY-MM-DD",
+//   "valueAxes": [ {
+//     "position": "left"
+//   } ],
+//   "graphs": [ {
+//     "id": "g1",
+//     "proCandlesticks": true,
+//     "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
+//     "closeField": "close",
+//     "fillColors": "#7f8da9",
+//     "highField": "high",
+//     "lineColor": "#7f8da9",
+//     "lineAlpha": 1,
+//     "lowField": "low",
+//     "fillAlphas": 0.9,
+//     "negativeFillColors": "#db4c3c",
+//     "negativeLineColor": "#db4c3c",
+//     "openField": "open",
+//     "title": "Price:",
+//     "type": "candlestick",
+//     "valueField": "close"
+//   } ],
+//   "chartScrollbar": {
+//     "graph": "g1",
+//     "graphType": "line",
+//     "scrollbarHeight": 30
+//   },
+//   "chartCursor": {
+//     "valueLineEnabled": true,
+//     "valueLineBalloonEnabled": true
+//   },
+//   "categoryField": "date",
+//   "categoryAxis": {
+//     "parseDates": true
+//   },
+//   "dataProvider": chartData,
+//   "export": {
+//     "enabled": true,
+//     "position": "bottom-right"
+//   }
+// } );
+//
+//   chart.addListener( "rendered", zoomChart );
+//   zoomChart();
+//   // this method is called when chart is first inited as we listen for "dataUpdated" event
+//   function zoomChart() {
+//     // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+//     chart.zoomToIndexes( chart.dataProvider.length - 50, chart.dataProvider.length - 1 );
+//   }
+// }
 
 
 // function generateChart(chartData)  {
