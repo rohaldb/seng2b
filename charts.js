08d4d681a7195f571161f215867cdf4cc09b2a81
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-05 17:32:28 +1000

    candelstick added

--- charts.js
+++ charts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     // $.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
+    getStockPriceOf("amzn");
 
     $("#search-stock").click(function() {
       var stock = $("#stock-name").val();
@@ -11,7 +12,9 @@ $(document).ready(function(){
 
 function getStockPriceOf(code) {
   $.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + code + "&interval=30min&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
+      console.log(data);
       var chartData = generateChartData(data);
+
   });
 }
 
@@ -22,6 +25,10 @@ function generateChartData(data) {
           chartData.unshift( {
             "date": key,
             "value": parseFloat(val["1. open"]),
+            "open": parseFloat(val["1. open"]),
+            "high": parseFloat(val["2. high"]),
+            "low": parseFloat(val["3. low"]),
+            "close" : parseFloat(val["4. close"]),
             "volume": parseFloat(val["5. volume"])
           } );
     });
@@ -30,108 +37,155 @@ function generateChartData(data) {
   generateChart(chartData);
 }
 
-function generateChart(chartData)  {
-  console.log(chartData);
 
+function generateChart(chartData) {
   var chart = AmCharts.makeChart( "chartdiv", {
-    "type": "stock",
-    "theme": "light",
-    "categoryAxesSettings": {
-      "minPeriod": "mm"
-    },
-
-    "dataSets": [ {
-      "color": "#b0de09",
-      "fieldMappings": [ {
-        "fromField": "value",
-        "toField": "value"
-      }, {
-        "fromField": "volume",
-        "toField": "volume"
-      } ],
-
-      "dataProvider": chartData,
-      "categoryField": "date"
-    } ],
-
-    "panels": [ {
-      "showCategoryAxis": false,
-      "title": "Value",
-      "percentHeight": 70,
-
-      "stockGraphs": [ {
-        "id": "g1",
-        "valueField": "value",
-        "type": "smoothedLine",
-        "lineThickness": 2,
-        "bullet": "round"
-      } ],
-
-
-      "stockLegend": {
-        "valueTextRegular": " ",
-        "markerType": "none"
-      }
-    }, {
-      "title": "Volume",
-      "percentHeight": 30,
-      "stockGraphs": [ {
-        "valueField": "volume",
-        "type": "column",
-        "cornerRadiusTop": 2,
-        "fillAlphas": 1
-      } ],
-
-      "stockLegend": {
-        "valueTextRegular": " ",
-        "markerType": "none"
-      }
-    } ],
-
-    "chartScrollbarSettings": {
-      "graph": "g1",
-      "usePeriod": "10mm",
-      "position": "top"
-    },
-
-    "chartCursorSettings": {
-      "valueBalloonsEnabled": true
-    },
-
-    "periodSelector": {
-      "position": "top",
-      "dateFormat": "YYYY-MM-DD JJ:NN",
-      "inputFieldWidth": 150,
-      "periods": [ {
-        "period": "hh",
-        "count": 1,
-        "label": "1 hour"
-      }, {
-        "period": "hh",
-        "count": 2,
-        "label": "2 hours"
-      }, {
-        "period": "hh",
-        "count": 5,
-        "selected": true,
-        "label": "5 hour"
-      }, {
-        "period": "hh",
-        "count": 12,
-        "label": "12 hours"
-      }, {
-        "period": "MAX",
-        "label": "MAX"
-      } ]
-    },
-
-    "panelsSettings": {
-      "usePrefixes": true
-    },
-
-    "export": {
-      "enabled": true,
-      "position": "bottom-right"
-    }
-  } );
+  "type": "serial",
+  "theme": "light",
+  "dataDateFormat":"YYYY-MM-DD",
+  "valueAxes": [ {
+    "position": "left"
+  } ],
+  "graphs": [ {
+    "id": "g1",
+    "proCandlesticks": true,
+    "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
+    "closeField": "close",
+    "fillColors": "#7f8da9",
+    "highField": "high",
+    "lineColor": "#7f8da9",
+    "lineAlpha": 1,
+    "lowField": "low",
+    "fillAlphas": 0.9,
+    "negativeFillColors": "#db4c3c",
+    "negativeLineColor": "#db4c3c",
+    "openField": "open",
+    "title": "Price:",
+    "type": "candlestick",
+    "valueField": "close"
+  } ],
+  "chartScrollbar": {
+    "graph": "g1",
+    "graphType": "line",
+    "scrollbarHeight": 30
+  },
+  "chartCursor": {
+    "valueLineEnabled": true,
+    "valueLineBalloonEnabled": true
+  },
+  "categoryField": "date",
+  "categoryAxis": {
+    "parseDates": true
+  },
+  "dataProvider": chartData,
+  "export": {
+    "enabled": true,
+    "position": "bottom-right"
+  }
+} );
 }
+
+// function generateChart(chartData)  {
+//   console.log(chartData);
+//
+//   var chart = AmCharts.makeChart( "chartdiv", {
+//     "type": "stock",
+//     "theme": "light",
+//     "categoryAxesSettings": {
+//       "minPeriod": "mm"
+//     },
+//
+//     "dataSets": [ {
+//       "color": "#b0de09",
+//       "fieldMappings": [ {
+//         "fromField": "value",
+//         "toField": "value"
+//       }, {
+//         "fromField": "volume",
+//         "toField": "volume"
+//       } ],
+//       "dataProvider": chartData,
+//       "categoryField": "date"
+//     } ],
+//
+//     "panels": [ {
+//       "showCategoryAxis": false,
+//       "title": "Value",
+//       "percentHeight": 70,
+//
+//       "stockGraphs": [ {
+//         "id": "g1",
+//         "valueField": "value",
+//         "type": "smoothedLine",
+//         "lineThickness": 2,
+//         "bullet": "round"
+//       } ],
+//
+//
+//       "stockLegend": {
+//         "valueTextRegular": " ",
+//         "markerType": "none"
+//       }
+//     }, {
+//       "title": "Volume",
+//       "percentHeight": 30,
+//       "stockGraphs": [ {
+//         "valueField": "volume",
+//         "type": "column",
+//         "cornerRadiusTop": 2,
+//         "fillAlphas": 1
+//       } ],
+//
+//       "stockLegend": {
+//         "valueTextRegular": " ",
+//         "markerType": "none"
+//       }
+//     } ],
+//
+//     "chartScrollbarSettings": {
+//       "graph": "g1",
+//       "usePeriod": "10mm",
+//       "position": "top"
+//     },
+//
+//     "chartCursorSettings": {
+//       "valueBalloonsEnabled": true
+//     },
+//
+//     "periodSelector": {
+//       "position": "top",
+//       "dateFormat": "YYYY-MM-DD JJ:NN",
+//       "inputFieldWidth": 150,
+//       "periods": [ {
+//         "period": "hh",
+//         "count": 1,
+//         "label": "1 hour"
+//       }, {
+//         "period": "hh",
+//         "count": 2,
+//         "label": "2 hours"
+//       }, {
+//         "period": "hh",
+//         "count": 5,
+//         "selected": true,
+//         "label": "5 hour"
+//       }, {
+//         "period": "hh",
+//         "count": 12,
+//         "label": "12 hours"
+//       }, {
+//         "period": "MAX",
+//         "label": "MAX"
+//       } ]
+//     },
+//
+//     "panelsSettings": {
+//       "usePrefixes": true
+//     },
+//
+//     "export": {
+//       "enabled": true,
+//       "position": "bottom-right"
+//     }
+//   } );
+// }
