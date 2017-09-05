9401a7dc0d30b2c4188a7400336d1439116ce5b8
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-05 18:01:18 +1000

    styling update

--- charts.js
+++ charts.js
@@ -1,14 +1,3 @@
-$(document).ready(function(){
-    // $.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
-    getStockPriceOf("amzn");
-
-    $("#search-stock").click(function() {
-      var stock = $("#stock-name").val();
-      console.warn("searching for " + stock);
-      getStockPriceOf(stock);
-    });
-
-});
 
 function getStockPriceOf(code) {
   $.get("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" + code + "&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 
 $(document).ready(function(){
-    // $.get("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
+  $("#search-stock").click(function() {
+    var stock = $("#stock-name").val();
+    console.warn("searching for " + stock);
+    getStockPriceOf(stock);
+  });
+  getStockPriceOf("amzn");
 
-    // $("#search-stock").click(function() {
-    //   var stock = $("#stock-name").val();
-    //   console.warn("searching for " + stock);
-    //   // getStockPriceOf(stock);
-    // });
-    //
-    // function getStockPriceOf(code) {
-    //   $.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=" + code + "&interval=1min&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
-    //       alert("Data: " + data + "\nStatus: " + status);
-    //       // console.log(data["Time Series (1min)"]);
-    //       return data;
-    //   });
-    // }
 });
