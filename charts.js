2cd02701ab4f233a0fa04eb8d4cdc13548455154
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-11 21:02:50 +1000

    tabbed graphs

--- charts.js
+++ charts.js
@@ -4,14 +4,19 @@ function getStockPriceOf(stockInfo) {
   $.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&interval=15min&symbol=" + code + "&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
     console.log(data);
     data = Object.values(data)[1];
-    var chartData = generateChartData(data);
+    var chartData = generateChartData(data, 1);
     console.warn(stockInfo);
     $("#company-name").text(stockInfo.Name);
     $("#company-price").text(chartData[chartData.length - 1].close);
   });
+  $.get("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=" + code + "&apikey=2V4IGWVZ6W8XS8AI", function(data, status){
+    console.log(data);
+    data = Object.values(data)[1];
+    var chartData = generateChartData(data, 2);
+  });
 }
 
-function generateChartData(data) {
+function generateChartData(data, type) {
   var chartData = [];
   //extract date from object
   $(data).each(function(i,val){
@@ -27,139 +32,140 @@ function generateChartData(data) {
           } );
     });
   });
-  generateChart(chartData);
+  if (type == 1) { generateIntradayChart(chartData);}
+  if (type == 2) { generateCandlestickChart(chartData); generateDrawableChart(chartData);}
   return chartData;
 }
 
 
-// function generateChart(chartData) {
-//   var chart = AmCharts.makeChart( "chartdiv1", {
-//   "type": "stock",
-//   "theme": "light",
-//   "dataSets": [ {
-//     "color": "#b0de09",
-//     "fieldMappings": [ {
-//       "fromField": "value",
-//       "toField": "value"
-//     } ],
-//     "dataProvider": chartData,
-//     "categoryField": "date"
-//   } ],
-//
-//   "panels": [ {
-//     "showCategoryAxis": true,
-//     "title": "Value",
-//     "eraseAll": false,
-//     "allLabels": [ {
-//       "x": 0,
-//       "y": 115,
-//       "text": "Click on the pencil icon on top-right to start drawing",
-//       "align": "center",
-//       "size": 16
-//     } ],
-//
-//     "stockGraphs": [ {
-//       "id": "g1",
-//       "valueField": "value",
-//       "useDataSetColors": false
-//     } ],
-//
-//     "stockLegend": {
-//       "valueTextRegular": " ",
-//       "markerType": "none"
-//     },
-//
-//     "drawingIconsEnabled": true
-//   } ],
-//
-//   "chartScrollbarSettings": {
-//     "graph": "g1"
-//   },
-//   "chartCursorSettings": {
-//     "valueBalloonsEnabled": true
-//   },
-//   "periodSelector": {
-//     "position": "bottom",
-//     "periods": [ {
-//       "period": "DD",
-//       "count": 10,
-//       "label": "10 days"
-//     }, {
-//       "period": "MM",
-//       "count": 1,
-//       "label": "1 month"
-//     }, {
-//       "period": "YYYY",
-//       "count": 1,
-//       "label": "1 year"
-//     }, {
-//       "period": "YTD",
-//       "label": "YTD"
-//     }, {
-//       "period": "MAX",
-//       "label": "MAX"
-//     } ]
-//   }
-// } );
-// }
+function generateCandlestickChart(chartData) {
+  var chart = AmCharts.makeChart( "chartdiv3", {
+  "type": "stock",
+  "theme": "light",
+  "dataSets": [ {
+    "color": "#b0de09",
+    "fieldMappings": [ {
+      "fromField": "value",
+      "toField": "value"
+    } ],
+    "dataProvider": chartData,
+    "categoryField": "date"
+  } ],
 
-// function generateChart(chartData) {
-//   var chart = AmCharts.makeChart( "chartdiv1", {
-//   "type": "serial",
-//   "theme": "light",
-//   "dataDateFormat":"YYYY-MM-DD",
-//   "valueAxes": [ {
-//     "position": "left"
-//   } ],
-//   "graphs": [ {
-//     "id": "g1",
-//     "proCandlesticks": true,
-//     "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
-//     "closeField": "close",
-//     "fillColors": "#7f8da9",
-//     "highField": "high",
-//     "lineColor": "#7f8da9",
-//     "lineAlpha": 1,
-//     "lowField": "low",
-//     "fillAlphas": 0.9,
-//     "negativeFillColors": "#db4c3c",
-//     "negativeLineColor": "#db4c3c",
-//     "openField": "open",
-//     "title": "Price:",
-//     "type": "candlestick",
-//     "valueField": "close"
-//   } ],
-//   "chartScrollbar": {
-//     "graph": "g1",
-//     "graphType": "line",
-//     "scrollbarHeight": 30
-//   },
-//   "chartCursor": {
-//     "valueLineEnabled": true,
-//     "valueLineBalloonEnabled": true
-//   },
-//   "categoryField": "date",
-//   "categoryAxis": {
-//     "parseDates": true
-//   },
-//   "dataProvider": chartData,
-//   "export": {
-//     "enabled": true,
-//     "position": "bottom-right"
-//   }
-// } );
-//
-//   chart.addListener( "rendered", zoomChart );
-//   zoomChart();
-//   // this method is called when chart is first inited as we listen for "dataUpdated" event
-//   function zoomChart() {
-//     // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
-//     chart.zoomToIndexes( chart.dataProvider.length - 50, chart.dataProvider.length - 1 );
-//   }
-// }
+  "panels": [ {
+    "showCategoryAxis": true,
+    "title": "Value",
+    "eraseAll": false,
+    "allLabels": [ {
+      "x": 0,
+      "y": 115,
+      "text": "Click on the pencil icon on top-right to start drawing",
+      "align": "center",
+      "size": 16
+    } ],
+
+    "stockGraphs": [ {
+      "id": "g1",
+      "valueField": "value",
+      "useDataSetColors": false
+    } ],
+
+    "stockLegend": {
+      "valueTextRegular": " ",
+      "markerType": "none"
+    },
+
+    "drawingIconsEnabled": true
+  } ],
+
+  "chartScrollbarSettings": {
+    "graph": "g1"
+  },
+  "chartCursorSettings": {
+    "valueBalloonsEnabled": true
+  },
+  "periodSelector": {
+    "position": "bottom",
+    "periods": [ {
+      "period": "DD",
+      "count": 10,
+      "label": "10 days"
+    }, {
+      "period": "MM",
+      "count": 1,
+      "label": "1 month"
+    }, {
+      "period": "YYYY",
+      "count": 1,
+      "label": "1 year"
+    }, {
+      "period": "YTD",
+      "label": "YTD"
+    }, {
+      "period": "MAX",
+      "label": "MAX"
+    } ]
+  }
+} );
+}
+
+function generateDrawableChart(chartData) {
+  var chart = AmCharts.makeChart( "chartdiv2", {
+  "type": "serial",
+  "theme": "light",
+  "dataDateFormat":"YYYY-MM-DD",
+  "valueAxes": [ {
+    "position": "left"
+  } ],
+  "graphs": [ {
+    "id": "g1",
+    "proCandlesticks": true,
+    "balloonText": "Open:<b>[[open]]</b><br>Low:<b>[[low]]</b><br>High:<b>[[high]]</b><br>Close:<b>[[close]]</b><br>",
+    "closeField": "close",
+    "fillColors": "#7f8da9",
+    "highField": "high",
+    "lineColor": "#7f8da9",
+    "lineAlpha": 1,
+    "lowField": "low",
+    "fillAlphas": 0.9,
+    "negativeFillColors": "#db4c3c",
+    "negativeLineColor": "#db4c3c",
+    "openField": "open",
+    "title": "Price:",
+    "type": "candlestick",
+    "valueField": "close"
+  } ],
+  "chartScrollbar": {
+    "graph": "g1",
+    "graphType": "line",
+    "scrollbarHeight": 30
+  },
+  "chartCursor": {
+    "valueLineEnabled": true,
+    "valueLineBalloonEnabled": true
+  },
+  "categoryField": "date",
+  "categoryAxis": {
+    "parseDates": true
+  },
+  "dataProvider": chartData,
+  "export": {
+    "enabled": true,
+    "position": "bottom-right"
+  }
+} );
+
+  chart.addListener( "rendered", zoomChart );
+  zoomChart();
+  // this method is called when chart is first inited as we listen for "dataUpdated" event
+  function zoomChart() {
+    // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+    chart.zoomToIndexes( chart.dataProvider.length - 50, chart.dataProvider.length - 1 );
+  }
+}
 
 
-function generateChart(chartData)  {
+function generateIntradayChart(chartData)  {
   var chart = AmCharts.makeChart( "chartdiv1", {
     "type": "stock",
     "theme": "light",
