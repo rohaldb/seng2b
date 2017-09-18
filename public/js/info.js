137b62af12109155e8d7fe6a0c70fab020bc49a9
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-09-18 23:52:28 +1000

    wiki/info tab mostly done

--- public/js/info.js
+++ public/js/info.js
@@ -21,7 +21,10 @@ function getCompanyInfo(company) {
       var components = extract.split('.');
       var i = 0;
 
-      while (len < 50) {
+      while (len < 250) {
+        if (!components[i].match(/[^\s]+\s/)) {
+          break;
+        }
         summary += components[i] + '.';
         len = summary.length;
         i++;
@@ -36,7 +39,7 @@ function getCompanyInfo(company) {
   //extract wiki infobox entries
   $.ajax({
     type: "GET",
-    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=amazon.com&callback=?",
+    url: 'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&page=' + company + '&callback=?',
     contentType: "application/json; charset=utf-8",
     async: false,
     dataType: "json",
@@ -47,19 +50,39 @@ function getCompanyInfo(company) {
       markup = markup.replace(/<[^>]*>/g, '');
 
       //not highly robust
-      var industry = markup.match(/Industry.*\n(.*)/g)[0].replace(/Industry/, '');
+      var industry = markup.match(/Industry.*(\n)*(.*)/g)[0].replace(/Industry/, '');
       var founded = markup.match(/Founded.*\n(.*)/g)[0].match(/.*?[0-9]{4}/);
       var founder = markup.match(/Founder.*\n(.*)/g)[0];
       var headquarters = markup.match(/Headquarters.*\n(.*)/g)[0].replace(/Headquarters/i, '');
-      var ceo = markup.match(/Key People(.|\n)*/gi)[0].replace(/\([^(]*CEO(.|\n)*/, '').replace(/Key People/i, '');
-      var employees = markup.match(/Number of Employees.*\n.*\n(.*)/gi)[0].match(/[0-9,]+/);
+      var ceo = markup.match(/Key People(.|\n)*/gi)[0].replace(/\([^(]*CEO(.|\n)*/, '').replace(/(.|\n)*\)/, '').replace(/Key People/i, '');
+      var employees = markup.match(/Number of Employees.*(\n)*(.*)/gi)[0].replace(/[,;]\s+.*/, '').replace(/Number of Employees/i, '');
+      employees = employees.replace(/\[.*\]/, '').replace(/\(.*\)/, '');
 
+      //at a glance...
       $('#industry').html(industry);
       $('#founded').html(founded);
       $('#founder').html(founder);
       $('#headquarters').html(headquarters);
       $('#ceo').html(ceo);
       $('#employees').html(employees);
+
+      //finances...
+      var revenue = markup.match(/Revenue.*\n.*\n(.*)/g)[0].replace(/Revenue/, '');
+      var year = revenue.match(/[0-9]{4}/)[0];
+      revenue = revenue.replace(/\[.*\]/, '').replace(/\(.*\)/, '');
+      $('#finances-year').html('Finances (' + year + ')');
+      $('#revenue').html(revenue);
+
+      markup = markup.replace(/\[.*\]/g, '').replace(/\(.*\)/g, '');
+      var operating = markup.match(/Operating Income.*\n.*\n(.*)/gi)[0].replace(/Operating Income/i, '');
+      $('#operating-income').html(operating);
+      var net = markup.match(/Net Income.*\n.*\n(.*)/gi)[0].replace(/Net Income/i, '');
+      $('#net-income').html(net);
+      var total = markup.match(/Total Assets.*\n.*\n(.*)/gi)[0].replace(/Total Assets/i, '');
+      $('#total-assets').html(total);
+      var equity = markup.match(/Total Equity.*\n.*\n(.*)/gi)[0].replace(/Total Equity/i, '');
+      $('#total-equity').html(equity);
+
     },
     error: function (errorMessage) {
     }
