e2621309ec9c43f778bcffbc3f0f0625fd666c65
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-19 14:41:43 +1000

    redirecting to company stock page on search finish

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
         // Callback function when value is autcompleted.
         console.log(companies[val]);
         console.warn("Searching for stock " + companies[val].Name);
-        getStockPriceOf(companies[val].Symbol);
+        window.location.replace("http://localhost:3000/stock?stock=" + companies[val].Symbol + "&company=" + companies[val].Name.replace(" ", "+"));
+        // getStockPriceOf(companies[val].Symbol);
       },
       minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
     });
