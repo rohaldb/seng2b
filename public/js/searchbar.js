3f792ee9a0457084a323356243622b848a5e2421
rohaldbUni <z5019999@ad.unsw.edu.au>
2017-09-19 14:21:25 +1000

    autocomplete working

--- public/js/searchbar.js
+++ public/js/searchbar.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
-$('input.autocomplete').autocomplete({
-    data: company_keys,
-    limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
-    onAutocomplete: function(val) {
-      // Callback function when value is autcompleted.
-      console.log(companies[val]);
-      console.warn("Searching for stock " + companies[val].Name);
-      getStockPriceOf(companies[val].Symbol);
-    },
-    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  $(function() {
+    $('input.autocomplete').autocomplete({
+      data: company_keys,
+      limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
+      onAutocomplete: function(val) {
+        // Callback function when value is autcompleted.
+        console.log(companies[val]);
+        console.warn("Searching for stock " + companies[val].Name);
+        getStockPriceOf(companies[val].Symbol);
+      },
+      minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+    });
   });
 });
