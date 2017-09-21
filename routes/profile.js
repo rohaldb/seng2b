257bf210f496030c03ea26ff67a442290dc1396c
Dominic Fung <domokun997@gmail.com>
2017-09-21 21:29:17 +1000

    Generate fake charts for user portfolio

--- routes/profile.js
+++ routes/profile.js
@@ -18,7 +18,7 @@ router.get('/', function(req, res, next) {
             "amcharts/amstock.js",
             "https://www.amcharts.com/lib/3/plugins/export/export.min.js",
             // Local files
-            "js/stock.js",
+            "js/profile.js",
         ],
     });
 });
