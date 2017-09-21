f502126e21ad1c2d9976248eba2e965532c774ab
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-09-21 22:45:53 +1000

    fix all titles & link sign out to landing

--- routes/landing.js
+++ routes/landing.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('landing', { title: 'Landing', layout: false});
+    res.render('landing', { title: 'NewStock', layout: false});
 });
 
 module.exports = router;
