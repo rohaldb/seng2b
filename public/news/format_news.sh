bfe20a022ed6dab7ba0fffd07d3d3267e2630277
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-09-25 17:47:11 +1000

    live company news working

--- public/news/format_news.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#Add paragraph tags to news articles
-
-for file in *.html
-do
-	sed -i '1 s/\(.*\)/<p>\n\1/' "$file"
-	sed -i 's/^$/<\/p>\n<p>/' "$file"
-	sed -i '$ s/\(.*\)/\1\n<\/p>/' "$file"
-done
