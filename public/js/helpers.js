588d8084e0232380d903765da8473f561adb25d0
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-10-25 14:40:56 +1100

    better timestamp heuristics + tooltips

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -2,7 +2,11 @@ function timeSince(timeStamp) {
   var now = new Date(),
     secondsPast = (now.getTime() - timeStamp.getTime()) / 1000;
   if(secondsPast < 60){
-    return parseInt(secondsPast) + 's ago';
+    if (secondsPast < 3) {
+      return 'Just now';
+    } else {
+      return parseInt(secondsPast) + 's ago';
+    }
   }
   if(secondsPast < 3600){
     return parseInt(secondsPast/60) + 'm ago';
@@ -12,14 +16,14 @@ function timeSince(timeStamp) {
   }
   if(secondsPast > 86400){
     var day = timeStamp.getDate();
-    var month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ","");
-    var year = timeStamp.getFullYear() == now.getFullYear() ? "" :  " "+timeStamp.getFullYear();
+    var month = timeStamp.toDateString().match(/ [a-zA-Z]*/)[0].replace(" ", "");
+    var year = timeStamp.getFullYear() == now.getFullYear() ? "" :" " + timeStamp.getFullYear();
     var hours = timeStamp.getHours();
     var am_pm = hours >= 12 ? 'pm' : 'am';
     hours %= 12;
     hours = hours ? hours : 12;
     var minutes = timeStamp.getMinutes();
-    minutes = minutes < 10 ? '0' + minutes : minutes;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
 
     return day + " " + month + year + " at " + hours + ":" + minutes + " " + am_pm;
   }
